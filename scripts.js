45f248210756d27c24d1c917a0089ecc3f0cbcdf
Justyna G <justykag@wp.pl>
2017-07-05 12:55:40 +0200

    Funkcje

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function getTriangleArea(a, h) {
+		
+	
+	if ((a, h) > 0){
+		
+	} else {
+		console.log("Nieprawidłowe dane");
+	}
+
+	return a * h / 2;
+}
+
+console.log( getTriangleArea(10,6) );
+
+var triangle1Area = getTriangleArea(48, 135);
+console.log(triangle1Area);
+
+var triangle1Area = getTriangleArea(77, 215);
+console.log(triangle1Area);
+
+var triangle1Area = getTriangleArea(87, -15);
+console.log(triangle1Area);
+
+
+
