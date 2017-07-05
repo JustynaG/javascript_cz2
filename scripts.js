16fc90a7ab75b76f4e50eec98bc4cd096099181a
Justyna G <justykag@wp.pl>
2017-07-05 21:55:41 +0200

    tablice

--- scripts.js
+++ scripts.js
@@ -22,4 +22,20 @@ var triangle1Area = getTriangleArea(87, -15);
 console.log(triangle1Area);
 
 
+// Zadanie 9.2 Tablice
+
+var fameleNames = [ 'Maria', 'Maja', 'Małgosia', 'Magda' , 'Milena'];
+var maleNames = [ 'Krzyś', 'Kuba', 'Kacper', 'Konrad', 'Karol']
+
+var allNames = fameleNames.concat(fameleNames, maleNames);
+console.log(allNames);
+
+var newName = 'Marian'
+
+	if (allNames.indexOf(newName) === -1){
+		var x = allNames.push('Marian');
+	}
+
+
+
 
