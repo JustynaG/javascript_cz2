4a52f23c2426a198e95fc1d6ccdf60fdd61f9866
Justyna G <justykag@wp.pl>
2017-07-13 10:48:58 +0200

    eventy

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
-
+add.addEventListener('click', function() {
+	var element = document.createElement('li');
+	element.innerHTML = 'item ' + (document.getElementsByTagName('li').length);
+	list.appendChild(element);
+});
 
 
